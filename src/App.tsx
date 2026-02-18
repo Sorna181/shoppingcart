c07612ff5e03a0a392b3114d10f78f717362b2b1
Sorna181 <sornalakshmi181@gmail.com>
2026-02-18 08:30:03 +0530

    Fix ProductGrid import to use named export

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ProductGrid from './components/ProductGrid';
+import { ProductGrid } from './components/ProductGrid';
 import ShoppingCart from './components/ShoppingCart';
 import SearchSection from './components/SearchSection';
 import SavingsDashboard from './components/SavingsDashboard';
